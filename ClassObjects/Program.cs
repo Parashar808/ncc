79d40957711f00f9b54f619edc1887d9b90f11c6
Parashar <baralpara4@gmail.com>
2022-04-28 20:58:35 +0545

    data type properties get set

---
+++ ClassObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ClassObjects{
+    
+
+ public class Person
+{
+  private string name; // field
+  public string Name   // property
+  {
+    get { return name; }
+    set { 
+        if (string.IsNullOrWhiteSpace(value))
+                {
+                    value = "Babu";
+                }
+        name = value; }
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Person myObj = new Person();
+    myObj.Name = "";
+    Console.WriteLine(myObj.Name);
+    return;
+  }
+}
+      
+      
+}
