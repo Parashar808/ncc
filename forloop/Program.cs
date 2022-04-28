571d3820984317e8e04eced113398c9dde35e9bd
Parashar <baralpara4@gmail.com>
2022-04-28 08:10:27 +0545

    added string functions

--- forloop/Program.cs
+++ forloop/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text;
 namespace forloop{
     public class program{
         public static void Main(){
             stringmax();
             usingfor();
+            stringformat();
             return;
         }
     public static void stringmax(){
@@ -20,6 +22,32 @@ namespace forloop{
             Console.WriteLine("days="+days[i]);
 
         }
+    
+    }
+    public static void stringformat(){
+        string x="Anjal";
+        string y="Thapa";
+        Console.WriteLine(string.Format("{0} {1}",x,y));
+
+        //string interpolation
+        string a="hello";
+        string b="nicky";
+
+        string res=$"{a}! {b}" ;
+        Console.WriteLine(res);
+
+        //string builder (using System.Text;)
+
+        StringBuilder abc= new StringBuilder();
+        abc.AppendLine("This is a ");
+        abc.AppendLine("appended line.");
+        string res1=abc.ToString();
+        Console.WriteLine(res1);
+
+
+
+        
+        return;
 
     }
     }
