fe95921c42d5177fbc7500ae53b9e52632dc8923
Parashar <baralpara4@gmail.com>
2022-05-18 14:22:15 +0545

    inheritance using base

---
+++ inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace inheritance{
+    public class program{
+
+        public static void Main(){
+            var a2= new class2(4); //this prints from both classes
+            
+
+        }
+
+
+    }
+
+    public class class1{
+        public class1(){
+            
+        }
+
+        
+           
+
+            public class1(int i){
+
+                            Console.WriteLine($"the number of class1 is {i}");
+
+
+            }
+            
+
+       
+        
+
+    }
+    public class class2: class1{
+
+        public class2(){
+
+        }
+
+        public class2(int i):base(i){
+
+            Console.WriteLine($"the number of class2 is {i}");
+
+        }
+    }
+}
